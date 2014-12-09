50a8f64425cc40329e215a630926b911543b88a6
Victor Leung <victorleungtw@gmail.com>
2014-12-09 12:01:57 +0800

    upgrade three js version from 58 to 69

--- js/visualizer.js
+++ js/visualizer.js
@@ -12,47 +12,55 @@ var far = 10000;
 var $container = $('#container');
 
   // create a WebGL renderer, camera and a scene
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({alpha:true});
 var camera = new THREE.PerspectiveCamera(view_angle, aspect, near, far);
 var scene = new THREE.Scene();
 
 var cubes = [];
 var controls;
 
-var material = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
 
-var geometry = new THREE.CubeGeometry(2, 1.5, 6);
 
 var i = 0;
 for(var x = 0; x < 30; x += 2) {
-	var j = 0;
-	cubes[i] = [];
-	for(var y = 0; y < 30; y += 2) {
+  var j = 0;
+  cubes[i] = [];
+  for(var y = 0; y < 30; y += 2) {
+    var geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
+    var material = new THREE.MeshPhongMaterial({
+      color: randomFairColor(),
+      ambient: 0x808080,
+      specular: 0xffffff,
+      shininess: 20,
+      reflectivity: 5.5
+    });
 		cubes[i][j] = new THREE.Mesh(geometry, material);
-		cubes[i][j].position = new THREE.Vector3(x, y, 0);
+		cubes[i][j].position.x = x;
+    cubes[i][j].position.y = y;
+    cubes[i][j].position.z = 0;
 		scene.add(cubes[i][j]);
 		j++;
 	}
 	i++;
 }
   // create a point light
-var light = new THREE.SpotLight(0xffffff);
+var light = new THREE.DirectionalLight(0xffffff);
 scene.add(light);
 
-var light = new THREE.SpotLight(0xffffff, 0.7);
+var light = new THREE.DirectionalLight(0xffffff, 0.7);
 light.position.set(0, 1, 1);
 scene.add(light);
 
-light = new THREE.SpotLight(0xffffff, 0.7);
+light = new THREE.DirectionalLight(0xffffff, 0.7);
 light.position.set(1, 1, 0);
 scene.add(light);
 
 
-light = new THREE.SpotLight(0xffffff, 0.7);
+light = new THREE.DirectionalLight(0xffffff, 0.7);
 light.position.set(0, -1, -1);
 scene.add(light);
 
-light = new THREE.SpotLight(0xffffff, 0.7);
+light = new THREE.DirectionalLight(0xffffff, 0.7);
 light.position.set(-1, -1, 0);
 scene.add(light);
 
@@ -79,8 +87,8 @@ var render = function () {
 				boost += dataArray[i];
 				var scale = (dataArray[k] + boost) / 3000;
 				cubes[i][j].scale.z = (scale < 1 ? 1 : scale);
-				var red = scale * 50;
-				cubes[i][j].material.color.r = scale + 100;
+				// var red = scale * 50;
+				// cubes[i][j].material.color.r = scale + 100;
 				if(isNaN(k)){
 					k += 0;
 				}
@@ -99,7 +107,7 @@ var render = function () {
 renderer.setSize(width, height);
 
 // and make it pretty
-renderer.setClearColor(0x555753, 1.0);
+renderer.setClearColor(0xffffff, 1.0);
 renderer.clear();
 
 render();
