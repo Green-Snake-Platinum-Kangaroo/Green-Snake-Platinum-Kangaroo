794b7ae9097699971fb5cb48368b4d6af5b209ef
mcpike <patrick.mcpike@gmail.com>
2014-12-09 15:50:39 -0800

    refactored audio context setup and added comments

--- js/audio.js
+++ js/audio.js
@@ -11,16 +11,45 @@ var interval = window.setInterval(function() {
 	}
 }, 500);
 
+/*
+	The following section is the setup for routing Audio to the visualizer:
+
+	There are 5 general steps:
+
+	1. Create audio context
+	2. Inside the context, create sources — such as <audio>, oscillator, stream
+	3. Create effects nodes, such as reverb, biquad filter, panner, compressor
+	4. Choose final destination of audio, for example your system speakers
+	5. Connect the sources up to the effects, and the effects to the destination.
+
+	For more information see:	https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
+
+*/
+
+
+// 1. Create audio context, see: https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var analyser = audioCtx.createAnalyser();
-var audio  = document.querySelector('audio');
+
+// 2. Inside the context, create sources 
+var audio  = document.querySelector('audio'); // creating a reference to the <audio> tag on index.html
 audio.volume = 0.15;
 var source = audioCtx.createMediaElementSource(audio);
+
+// 3. Create effects nodes
+var analyser = audioCtx.createAnalyser();
+
+
+// 4. Choose final destination of audio
+analyser.connect(audioCtx.destination);
+
+// 5. Connect the sources up to the effects
 source.connect(analyser);
-var gainNode = audioCtx.createGain();
-source.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-analyser.fftSize = 256;
+
+
+
+
+analyser.fftSize = 256;  
+
 var bufferLength = analyser.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 
