edd3ec03569c29ff1eec044d94aa474a7660bb4f
Victor Leung <victorleungtw@gmail.com>
2014-12-10 12:15:12 +0800

    add one unit test for render index html page

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('index.html');
+  // response.send('index.html');
+  response.send("Hello Cybertron!");
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+module.exports.getApp = app;
