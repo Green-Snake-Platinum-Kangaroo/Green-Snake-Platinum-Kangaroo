8ee08e14674cd96e1b75e83f9dde8b823b65e717
Victor Leung <victorleungtw@gmail.com>
2014-12-10 09:05:22 +0800

    add node js server to render index page

---
+++ app.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000);
+
+console.log('Server started: http://localhost:3000/');
