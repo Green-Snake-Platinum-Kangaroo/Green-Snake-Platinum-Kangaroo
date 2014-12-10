b4b8be05de922f899106041aea315a49972481a1
Victor Leung <victorleungtw@gmail.com>
2014-12-10 09:25:06 +0800

    change server setup

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var fs = require('fs');
-var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+app.get('/', function(request, response) {
+  response.send('index.html');
+});
 
-console.log('Server started: http://localhost:3000/');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+// var fs = require('fs');
+// var path = require('path');
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+
+// app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// app.listen(3000);
+
+// console.log('Server started: http://localhost:3000/');
