94667733bef628d5fbf90d38a1b391e53bc398a7
mcpike <patrick.mcpike@gmail.com>
2014-12-10 15:16:08 -0800

    updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,80 @@
-module.exports = function(grunt) {
-  // Do grunt-related things in here
-};
+module.exports = function(grunt){
+
+  // grunt.initConfig({
+  //   pkg: grunt.file.readJSON('package.json'),
+  //   jshint: {
+  //           all: {
+  //               src: 'src/js/*.js',
+  //               options: {
+  //                   bitwise: true,
+  //                   camelcase: true,
+  //                   curly: true,
+  //                   eqeqeq: true,
+  //                   forin: true,
+  //                   immed: true,
+  //                   indent: 4,
+  //                   latedef: true,
+  //                   newcap: true,
+  //                   noarg: true,
+  //                   noempty: true,
+  //                   nonew: true,
+  //                   quotmark: 'single',
+  //                   regexp: true,
+  //                   undef: true,
+  //                   unused: true,
+  //                   trailing: true,
+  //                   maxlen: 120
+  //               }
+  //           }
+  //       },
+  //   concat: {
+  //     js: {
+  //       src: [
+  //         'src/js/*'
+  //       ],
+  //       dest: 
+  //         'src/tmp/single.js'
+  //     }
+  //   },
+  //   uglify: {
+  //     scripts: {
+  //       options: {
+  //         // the banner is inserted at the top of the output
+  //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+  //       },
+  //       files: {
+  //         'dist/js/output.min.js': ['src/tmp/single.js']
+  //       }
+  //     },
+  //     src: {
+  //       files: {
+  //         'src/tmp/script1.min.js': ['src/js/script1.js']
+  //       }
+  //     },
+  //     test: {
+  //       src: 'src/js/script2.js',
+  //       dest: 'src/tmp/script2.min.js'
+  //     }
+  //   },
+  //   watch: {
+  //     scripts: {
+  //       files: ['src/js/*.js'],
+  //       tasks: ['default'],
+  //       options: {
+  //         spawn: false,
+  //       },
+  //     },
+  //   },
+  // });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // grunt.registerTask('default', ['jshint', 'concat:js', 'uglify:scripts']);
+
+  grunt.registerTask("hello", function(){
+    grunt.log.writeln('Hello World!')
+  });
+}
